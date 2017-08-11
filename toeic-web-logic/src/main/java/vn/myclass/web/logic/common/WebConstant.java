dde95325742458630471b7d3c799a4a67973f8d0
Phan <mrquocphuong@gmail.com>
2017-08-11 13:19:08 +0700

    init project

---
+++ toeic-web-logic/src/main/java/vn/myclass/web/logic/common/WebConstant.java
@@ -0,0 +1,7 @@
+package vn.myclass.web.logic.common;
+
+/**
+ * Created by Phan on 8/11/2017.
+ */
+public class WebConstant {
+}
---
+++ toeic-web/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>TEST</title>
+</head>
+
+<body>
+    <h1>Test project</h1>
+</body>
+</html>
